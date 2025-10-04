b1e18a77bfb812f854a5b7fbba8b74f848cdbbb3
Schubert <schubert@demogr.mpg.de>
2025-10-04 16:16:26 +0200

    Initialize the fist commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Estimating male fertility schedules
---
+++ code/01_load_data.R
@@ -0,0 +1,7 @@
+###########
+# Project: Estimating male fertility
+# Purpose: Load and clean data
+# Author: Henrik-Alexander Schubert
+# E-mail: schubert@demogr.mpg.de
+# Date: 27.09.2024
+#################
